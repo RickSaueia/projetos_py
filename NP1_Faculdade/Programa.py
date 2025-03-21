334bf7bcc93c6b5d08a08e327f97a25fd94378e5
Leandro Rick <lele.saueia@gmail.com>
2025-03-21 12:23:42 -0300

    Adicionando funções do python e adicionando while

--- NP1_Faculdade/Programa.py
+++ NP1_Faculdade/Programa.py
@@ -1,72 +1,36 @@
 generos_filme = [
-    ["filme de ação 1","filme de ação 2","filme de ação 3"],
-    ["filme de comédia 1","filme de comédia 2","filme de comédia 3"],
-    ["filme de terror 1","filme de terror 2","filme de terror 3"],
+    ["filme de ação 1", "filme de ação 2", "filme de ação 3"],
+    ["filme de comédia 1", "filme de comédia 2", "filme de comédia 3"],
+    ["filme de terror 1", "filme de terror 2", "filme de terror 3"],
 ]
+
 sinopse_filme = [
-    ["sinopse de ação 1","sinopse de ação 2","sinopse de ação 3"],
-    ["sinopse de comédia 1","sinopse de comédia 2","sinopse de comédia 3"],
-    ["sinopse de terror","sinopse de terror 2","sinopse de terror 3"],
+    ["sinopse de ação 1", "sinopse de ação 2", "sinopse de ação 3"],
+    ["sinopse de comédia 1", "sinopse de comédia 2", "sinopse de comédia 3"],
+    ["sinopse de terror 1", "sinopse de terror 2", "sinopse de terror 3"],
 ]
-escolha_genero = input("Escolha um genero de filme para acessar a galeria. | [1] Ação | [2] Comédia | [3] Terror \n")
-print("_______________")
- 
-""" FILMES DE AÇÃO """
-if escolha_genero == "1":
-    print("Filmes de Ação:")
-    print(generos_filme[0])
-    print("\n")
-    escolha_filme = input("Deseja ver a descriçaõ de qual filme? [1] filme 1 [2] filme 2 [3] filme 3 \n")
-    if escolha_filme == "1":
-        print(sinopse_filme[0][0])
-        breakpoint()
-    if escolha_filme == "2":
-        print(sinopse_filme[0][1])
-        breakpoint()
-    if escolha_filme == "3":
-        print(sinopse_filme[0][2])
-        breakpoint()
+
+while True:
+    escolha_genero = input("\nEscolha um gênero de filme para acessar a galeria:\n[1] Ação\n[2] Comédia\n[3] Terror\n[0] Sair\n")
+    print("_______________")
+    
+    if escolha_genero == "0":
+        print("Saindo do programa...")
+        break
+
+    if escolha_genero in ["1", "2", "3"]:
+        indice_genero = int(escolha_genero) - 1
+        print(f"Filmes de {['Ação', 'Comédia', 'Terror'][indice_genero]}:")
+        for i, filme in enumerate(generos_filme[indice_genero], start=1):
+            print(f"[{i}] {filme}")
+            
+        while True:
+            escolha_filme = input("\nDeseja ver a descrição de qual filme? Digite o número ou [0] para voltar: ")
+            if escolha_filme == "0":
+                break
+            elif escolha_filme in ["1", "2", "3"]:
+                print(sinopse_filme[indice_genero][int(escolha_filme) - 1])
+            else:
+                print("Escolha inválida, tente novamente.")
     else:
-        print("escolha inválida")
- 
- 
-""" FILMES DE COMEDIA """
-if escolha_genero == "2":
-    print("Filmes de Comédia:\n")
-    print(generos_filme[1])
-    print("\n")
-    escolha_filme = input("Deseja ver a descriçaõ de qual filme? [1] filme 1 [2] filme 2 [3] filme 3 \n")
-    if escolha_filme == "1":
-        print(sinopse_filme[1][0])
-        breakpoint()
-    if escolha_filme == "2":
-        print(sinopse_filme[1][1])
-        breakpoint()
-    if escolha_filme == "3":
-        print(sinopse_filme[1][2])
-        breakpoint()
-    else:
-        print("escolha inválida")
-   
- 
-""" FILMES DE TERROR """
-if escolha_genero == "3":
-    print("Filmes de Terror:\n")
-    print(generos_filme[2])
-    print("\n")
-    escolha_filme = input("Deseja ver a descriçaõ de qual filme? [1] filme 1 [2] filme 2 [3] filme 3 \n")
-    if escolha_filme == "1":
-        print(sinopse_filme[2][0])
-        breakpoint()
-    if escolha_filme == "2":
-        print(sinopse_filme[2][1])
-        breakpoint()
-    if escolha_filme == "3":
-        print(sinopse_filme[2][2])
-        breakpoint()
-    else:
-        print("escolha inválida")
-   
-else:
-    print("escolha inválida")
- 
+        print("Escolha inválida, tente novamente.")
